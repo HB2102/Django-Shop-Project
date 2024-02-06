845da1ed32f53c5283f9c58d3a0594053d2b33fb
Hossein <hossienbehzadi1381@gmail.com>
2024-02-07 03:25:03 +0330

    starting the project

---
+++ MyShop/shop/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+import datetime
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500, default='', blank=True, null=True)
+    price = models.DecimalField(default=0, decimal_places=0, max_digits=15)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='upload/product/')
+
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(default='', blank=True, max_length=700)
+    phone = models.CharField(max_length=20, blank=True)
+    date = models.DateField(default=datetime.datetime.today())
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.customer} , {self.phone}, {self.status}'
---
+++ MyShop/shop/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+def hellowold(request):
+    return render(request, 'index.html')
+
