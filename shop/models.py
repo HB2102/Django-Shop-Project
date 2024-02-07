964867524c2e72ec5c5d6e5d553a6cbfcacc1d41
Hossein <hossienbehzadi1381@gmail.com>
2024-02-07 17:06:02 +0330

    changing directory and adding static

--- shop/models.py
+++ shop/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     price = models.DecimalField(default=0, decimal_places=0, max_digits=15)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to='upload/product/')
 
 
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
+
 def hellowold(request):
     return render(request, 'index.html')
 
