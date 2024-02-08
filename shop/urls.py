dc2badd430147b5df8b80a4735e2dbc7d5dac5ee
Hossein <hossienbehzadi1381@gmail.com>
2024-02-08 04:56:29 +0330

    adding login and logout and organizing templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import helloworld
+from .views import helloworld, about_us, login_user, logout_user
 
 urlpatterns = [
-    path('', helloworld)
+    path('', helloworld, name='home'),
+    path('about/', about_us, name='about'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def helloworld(request):
     all_products = Product.objects.all()
 
-
-
     return render(request, 'index.html', {'products': all_products})
 
+
+def about_us(request):
+    return render(request, 'about.html')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You logged in successfully.')
+            return redirect('home')
+        else:
+            messages.warning(request, 'Username or password is invalid')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('home')
