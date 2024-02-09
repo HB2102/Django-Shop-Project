be2feef61e50b50dffb1a509d02988ed18040749
Hossein <hossienbehzadi1381@gmail.com>
2024-02-09 04:13:38 +0330

    adding cart app and cart template and stuff

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
