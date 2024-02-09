aaedf493202a7c05ba3484e0bf0f504d7435c0ee
Hossein <hossienbehzadi1381@gmail.com>
2024-02-09 18:27:49 +0330

    adding cart item number show

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from shop.models import Product
+
 class Cart:
     def __init__(self, request):
         self.session = request.session
@@ -8,12 +10,27 @@ class Cart:
 
         self.cart = cart
 
-    def add(self, product):
+        self.session.modified = True
+
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_quantity = quantity
 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = product_quantity
 
-        self.session.modifies = True
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+
+    def get_prods(self):
+        prod_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=prod_ids)
+        return products
+
+    def get_quantity(self):
+        quantities = self.cart
+        return quantities
