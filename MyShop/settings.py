2d88ecf9b69a8c51403d342efede2e8732e6ecde
Hossein <hossienbehzadi1381@gmail.com>
2024-02-17 08:24:04 +0330

    add readme file

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -113,7 +113,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_URL = ['/static',]
+STATICFILES_URL = ['/static']
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
